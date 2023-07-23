ea9a15907db8ae0d8d960b24ab78416304da5e5e
胡鹏 <15151873042@163.com>
2023-07-23 23:10:38 +0800

    shengyu调整

--- nbb-cloud/nabb-cloud-shenyu/shenyu-client-springcloud-example/src/main/java/com/nbb/cloud/shenyu/ShenyuSpringcloudClientApplication.java
+++ nbb-cloud/nabb-cloud-shenyu/shenyu-client-springcloud-example/src/main/java/com/nbb/cloud/shenyu/ShenyuSpringcloudClientApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ShenyuProviderApplication {
+public class ShenyuSpringcloudClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShenyuProviderApplication.class, args);
+        SpringApplication.run(ShenyuSpringcloudClientApplication.class, args);
     }
 }
